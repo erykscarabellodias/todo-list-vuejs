92b914c11ff69c6614174fe0c3827c42ebb919df
erykscarabellodias <erykscarabello97@gmail.com>
2024-06-27 10:12:33 -0300

    iniciando projeto e criando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Content -->
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner v-if="loading" />
+
+      <template v-else>
+        <TodoFormAdd />
+
+        <TodoItems />
+
+        <TodoEmpty />
+
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from '@/components/TodoSpinner.vue'
+import TodoFormAdd from '@/components/TodoFormAdd.vue'
+import TodoItems from '@/components/TodoItems.vue'
+import TodoEmpty from '@/components/TodoEmpty.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty,
+  },
+
+  data() {
+    return {
+      loading: false
+    }
+  },
+
+  async created() {
+    this.loading = true
+    const todos = await axios.get('http://localhost:3000/todos');
+
+    this.$store.commit('storeTodos', todos.data)
+
+    this.loading = false;
   }
 }
 </script>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="space-y-2">
+    <TodoItem :key="todo.id" v-for="todo in $store.state.todos" :todo="todo" />
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+
+export default {
+  components: { TodoItem }
+}
+</script>
